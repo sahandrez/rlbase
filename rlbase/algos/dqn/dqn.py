32e067355576d7e188184a2c8aca5f6f51c532a2
Sahand Rezaei-Shoshtari <sahand.rezaei.sh@gmail.com>
2020-03-22 12:07:34 -0400

    Add Double Q-Learning to DQN implementation

--- rlbase/algos/dqn/dqn.py
+++ rlbase/algos/dqn/dqn.py
@@ -43,12 +43,11 @@ class ReplayBuffer:
         return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in batch.items()}
 
 
-def dqn(env_fn, policy=core.MLPQPolicy, policy_kwargs=dict(), seed=0,
-        steps_per_epoch=4000, epochs=100, replay_size=int(1e6),
-        gamma=0.99, polyak=0.995, q_lr=1e-3, batch_size=100,
-        start_steps=10000, update_after=1000, update_every=50,
-        num_test_episodes=10, eps_start=0.9, eps_end=0.05, eps_decay=200,
-        max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
+def dqn(env_fn, policy=core.MLPQPolicy, policy_kwargs=dict(), double_Q=True,
+        seed=0, steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99,
+        polyak=0.995, q_lr=1e-3, batch_size=100, start_steps=10000, update_after=1000,
+        update_every=50, num_test_episodes=10, eps_start=0.9, eps_end=0.05,
+        eps_decay=200, max_ep_len=1000, logger_kwargs=dict(), save_freq=1):
     """
     Deep Q Network (DQN)
 
@@ -78,6 +77,8 @@ def dqn(env_fn, policy=core.MLPQPolicy, policy_kwargs=dict(), seed=0,
 
         policy_kwargs (dict): Any kwargs appropriate for the ActorCritic object
             you provided to DDPG.
+
+        double_Q: If true, uses double Q-Learning.
 
         seed (int): Seed for random number generators.
 
@@ -170,8 +171,15 @@ def dqn(env_fn, policy=core.MLPQPolicy, policy_kwargs=dict(), seed=0,
 
         # Bellman backup for Q function
         with torch.no_grad():
-            # expected state action values of the next state
-            q_pi_target = q_target.q_net(o2).max(1)[0].detach()
+            if not double_Q:
+                # expected state action values of the next state
+                q_pi_target = q_target.q_net(o2).max(1)[0].detach()
+            else:
+                # in double Q-learning the action taken in the next state is chosen based on
+                # the policy network instead of the target network. The next action should be
+                # unsqueezed to have consistent size inside the gather method
+                next_state_action = q_policy.q_net(o2).argmax(1).unsqueeze(1)
+                q_pi_target = q_target.q_net(o2).gather(1, next_state_action.long()).squeeze(-1)
             backup = r + gamma * (1 - d) * q_pi_target
 
         # MSE loss against Bellman backup
@@ -298,6 +306,8 @@ if __name__ == '__main__':
     parser.add_argument('--hid', type=int, default=256)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--no_double_Q', action='store_true', default=False,
+                        help='If true, does not uses double Q-Learning algorithm.')
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--exp_name', type=str, default='dqn')
@@ -309,5 +319,5 @@ if __name__ == '__main__':
 
     dqn(lambda: gym.make(args.env), policy=core.MLPQPolicy,
         policy_kwargs=dict(hidden_sizes=[args.hid] * args.l),
-        gamma=args.gamma, seed=args.seed, epochs=args.epochs,
-        logger_kwargs=logger_kwargs)
+        double_Q=not args.no_double_Q, gamma=args.gamma, seed=args.seed,
+        epochs=args.epochs, logger_kwargs=logger_kwargs)
